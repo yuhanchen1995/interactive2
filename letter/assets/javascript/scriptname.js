8fcd2f89d5b62bcf4e0adf1498747f50f66eae03
Yuhan Chen <yuhan.chen@cca.edu>
2019-02-13 18:15:36 -0800

    hello

---
+++ letter/assets/javascript/scriptname.js
@@ -0,0 +1,9 @@
+ document.querySelector('.alphabet-button').onclick = function () {
+        location.href = "alphabet.html";
+    };
+document.querySelector('.name-button').onclick = function () {
+        location.href = "name.html";
+    };
+document.querySelector('.quote-button').onclick = function () {
+        location.href = "quote.html";
+    };
